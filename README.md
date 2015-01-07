18e57a4bf569a8606e0a1079fc8a85873acf322c
administrator <ayduno@gmail.com>
2015-01-07 21:09:14 +1100

    add example

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Needs a bit of work still.
 -Clone
 -Add URLs to core.php
 -Visit index.php
+
+Example at:
+https://www.aidan.in/phpfeedreader/
--- index.php
+++ index.php
@@ -14,28 +14,28 @@
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <div id=heading>
+  <div id="heading">
     <h1>PHP Feed Reader</h1>
   </div>
-  <div id=container>
+  <div id="container">
 <?php foreach ($BLOGARRAY as $BLOGITEM): ?>
     <div id="<?=$BLOGITEM->getBlogMd5()?>" class="post <?=$BLOGITEM->getPostDateTime()?>">
       <div class="inner">
-        <div class="post_header" style="background-image:url('https://www.google.com/s2/favicons?domain_url=<?=$BLOGITEM->getBlogURL()?>')">             
+        <div class="post_header" style="background-image:url(https://www.google.com/s2/favicons?domain_url=<?=$BLOGITEM->getBlogURL()?>)">             
           <h2><a href="<?=$BLOGITEM->getBlogURL()?>" target="_blank"><?=htmlspecialchars($BLOGITEM->getBlogTitle())?></a></h2>
-          <span class=post_date><i>- <?=$BLOGITEM->getPostRelativeDate()?> </i></span>
+          <span class="post_date"><i>- <?=$BLOGITEM->getPostRelativeDate()?> </i></span>
         </div>
-        <div class=post_contents>
-          <p class=post_title> <a href="<?=$BLOGITEM->getPostURL()?>" target="_blank"><?=$BLOGITEM->getPostTitle()?></a> </p>
-          <p class=post_description> <?=$BLOGITEM->getPostDescription($CHARLENGTH)?> </p>
+        <div class="post_contents">
+          <p class="post_title"> <a href="<?=$BLOGITEM->getPostURL()?>" target="_blank"><?=$BLOGITEM->getPostTitle()?></a> </p>
+          <p class="post_description"> <?=$BLOGITEM->getPostDescription($CHARLENGTH)?> </p>
         </div>  
       </div>
     </div>
 <?php endforeach; ?>
   </div>
-  <div id=footer>
-    <span id=updated>Updated <?=time_elapsed_string(filemtime("/tmp/" . $BLOGITEM->getBlogMd5()))?> (<a href="index.php?mode=update">update now</a>) - <a href="index.php?mode=json">JSON</a></span>
-    <span id=notice>Content and linked pages are copyright their respective owners</span>
+  <div id="footer">
+    <span id="updated">Updated <?=time_elapsed_string(filemtime("/tmp/" . $BLOGITEM->getBlogMd5()))?></span> (<a href="index.php?mode=update">update now</a>) - <a href="index.php?mode=json">JSON</a>
+    <span class="right">Content and linked pages are copyright their respective owners</span>
   </div>
 </body>
 </html>
